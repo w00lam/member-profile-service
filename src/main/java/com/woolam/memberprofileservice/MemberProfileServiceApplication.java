155b6044e76255a5c73b54bd842e3273758f37af
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-05-14 11:41:38 +0900

    chore: initialize spring boot project

---
+++ src/main/java/com/woolam/memberprofileservice/MemberProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.woolam.memberprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberProfileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woolam/memberprofileservice/MemberProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woolam.memberprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
